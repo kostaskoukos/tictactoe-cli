85314dcacdd41dabecfdb557f73d57dc4fb7dedd
kostaskoukos <kkonst2014@gmail.com>
2026-01-12 16:37:59 +0200

    add printBoard function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#include <array>
 #include <iostream>
 
+void printBoard(const std::array<char, 9> &board) {
+  for (size_t i = 0; i < 9; ++i) {
+    std::cout << board[i];
+
+    if (i == 8)
+      std::cout << "\n";
+    else if ((i + 1) % 3 == 0)
+      std::cout << "\n---------\n";
+    else
+      std::cout << " | ";
+  }
+}
+
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
+  std::array<char, 9> board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+  printBoard(board);
   return 0;
 }
