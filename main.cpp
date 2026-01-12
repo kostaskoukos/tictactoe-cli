e14bf5a32c794dfa81b9731ebd85df153e398e6f
kostaskoukos <kkonst2014@gmail.com>
2026-01-12 17:46:47 +0200

    implement Result enum and PvP game flow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <array>
 #include <iostream>
 
+enum Result { ONGOING, X_WINS, O_WINS, DRAW };
+
 void printBoard(const std::array<char, 9> &board) {
   for (size_t i = 0; i < 9; ++i) {
     std::cout << board[i];
@@ -14,9 +16,60 @@ void printBoard(const std::array<char, 9> &board) {
   }
 }
 
+Result calculateResult(const std::array<char, 9> &board) {
+  const int winPatterns[8][3] = {
+      {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Rows
+      {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columns
+      {0, 4, 8}, {2, 4, 6}             // Diagonals
+  };
+
+  for (const auto &pattern : winPatterns) {
+    if (board[pattern[0]] == board[pattern[1]] &&
+        board[pattern[1]] == board[pattern[2]]) {
+      if (board[pattern[0]] == 'X')
+        return X_WINS;
+      else if (board[pattern[0]] == 'O')
+        return O_WINS;
+    }
+  }
+  for (const char &cell : board) {
+    if (cell != 'X' && cell != 'O')
+      return ONGOING;
+  }
+  return DRAW;
+}
+
+
 int main(int argc, char *argv[]) {
   std::array<char, 9> board = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  char turn = 'X';
+  int move = 0;
+  Result res = ONGOING;
+
+  while (res == ONGOING) {
+    std::cout << "\033[2J\033[1;1H"; // clear screen
+    printBoard(board);
+    std::cout << "\n";
+
+    std::cout << turn << "'s turn.\nEnter your move (1-9):";
+    for (std::cin >> move; move < 1 || move > 9 || board[move - 1] == 'X' ||
+                           board[move - 1] == 'O';
+         std::cin >> move) {
+      std::cout << "Invalid move. Enter a new move (1-9):";
+    }
+
+    board[move - 1] = turn;
+    turn = (turn == 'X') ? 'O' : 'X';
+    res = calculateResult(board);
+  }
 
   printBoard(board);
+  if (res == X_WINS) {
+    std::cout << "X wins!\n";
+  } else if (res == O_WINS) {
+    std::cout << "O wins!\n";
+  } else {
+    std::cout << "It's a draw!\n";
+  }
   return 0;
 }
